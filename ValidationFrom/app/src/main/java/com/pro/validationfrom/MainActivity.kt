a66e1515529c8a176277628b836c353bfc77270f
sanjaydangar <sanjaydangar31@gmail.com>
2023-06-08 14:36:40 +0530

    add

--- ValidationFrom/app/src/main/java/com/pro/validationfrom/MainActivity.kt
+++ ValidationFrom/app/src/main/java/com/pro/validationfrom/MainActivity.kt
@@ -62,246 +62,65 @@ class MainActivity : AppCompatActivity() {
 
             if (firstName.isEmpty()) {
                 Toast.makeText(this, "Please Enter First Name", Toast.LENGTH_SHORT).show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
+                vibratorFun()
             } else if (lastName.isEmpty()) {
                 Toast.makeText(this, "Please Enter Last Name", Toast.LENGTH_SHORT).show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
+                vibratorFun()
             } else if (mobileNumber.isEmpty()) {
                 Toast.makeText(this, "Please Enter Mobile Number", Toast.LENGTH_SHORT).show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
+                vibratorFun()
             } else if (mobileNumber.length <= 9) {
                 Toast.makeText(this, "Please Enter Valid Mobile Number", Toast.LENGTH_SHORT).show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
+                vibratorFun()
             } else if (!Patterns.PHONE.matcher(mobileNumber).matches()) {
                 Toast.makeText(this, "Please Enter Valid Mobile Number", Toast.LENGTH_SHORT).show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
+                vibratorFun()
             } else if (email.isEmpty()) {
                 Toast.makeText(this, "Please Enter Email", Toast.LENGTH_SHORT).show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
+                vibratorFun()
             } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 Toast.makeText(this, "Please Enter Valid Email", Toast.LENGTH_SHORT).show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
+                vibratorFun()
             } else if (password.isEmpty()) {
                 Toast.makeText(this, "Please Enter Password", Toast.LENGTH_SHORT).show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
+                vibratorFun()
             } else if (password.length < 8) {
                 Toast.makeText(this, "Minimum 8 Character Password", Toast.LENGTH_SHORT).show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
+                vibratorFun()
             } else if (!password.matches(".*[A-Z].*".toRegex())) {
 
-                Toast.makeText(this, "Must Contain 1 Upper-case Character", Toast.LENGTH_SHORT)
-                    .show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
-
+                Toast.makeText(this, "Must Contain 1 Upper-case Character", Toast.LENGTH_SHORT).show()
+                vibratorFun()
             } else if (!password.matches(".*[a-z].*".toRegex())) {
-                Toast.makeText(this, "Must Contain 1 Lower-case Character", Toast.LENGTH_SHORT)
-                    .show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
-
+                Toast.makeText(this, "Must Contain 1 Lower-case Character", Toast.LENGTH_SHORT).show()
+                vibratorFun()
             } else if (!password.matches(".*[@#\$%^&+=].*".toRegex())) {
                 Toast.makeText(
                     this,
                     "Must Contain 1 Special Character (@#\$%^&+=)",
                     Toast.LENGTH_SHORT
                 ).show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
+                vibratorFun()
             } else if (confPassword.isEmpty()) {
                 Toast.makeText(
                     this,
                     "Please Enter Confirm Password",
                     Toast.LENGTH_SHORT
                 ).show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
+                vibratorFun()
 
             } else if (!confPassword.equals(password)) {
                 Toast.makeText(this, "Password do not match", Toast.LENGTH_SHORT).show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
+                vibratorFun()
             } else if (selectedId == -1) {
                 Toast.makeText(this, "Gender Not  selected ", Toast.LENGTH_SHORT).show()
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
+                vibratorFun()
 
 
-            } else if (!mainBinding.chkHobby1.isChecked && !mainBinding.chkHobby2.isChecked && !mainBinding.chkHobby3.isChecked) {
+            } else if (!mainBinding.chkHobby1.isChecked && !mainBinding.chkHobby2.isChecked && !mainBinding.chkHobby3.isChecked ){
                 Toast.makeText(this, "Hobby Not  selected ", Toast.LENGTH_SHORT).show()
-
-                var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator!!.vibrate(
-                        VibrationEffect.createOneShot(
-                            500,
-                            VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE
-                        )
-                    )
-                } else {
-                    //deprecated in API 26
-                    vibrator!!.vibrate(500)
-                }
-            } else {
+                vibratorFun()
+            }else {
 
                 var adapter: ArrayAdapter<*> =
                     ArrayAdapter<Any?>(
@@ -314,6 +133,18 @@ class MainActivity : AppCompatActivity() {
 
                 Toast.makeText(this, "Submit", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun vibratorFun() {
+        var vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator!!.vibrate(
+                VibrationEffect.createOneShot(500, VibrationEffect.PARCELABLE_WRITE_RETURN_VALUE)
+            )
+        } else {
+            //deprecated in API 26
+            vibrator!!.vibrate(500)
         }
     }
 }
